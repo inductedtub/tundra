a3fa7756f03eef24bfaddf8fb8c4b3075afed03b
Justin <carr0476@algonquinlive.com>
2019-04-09 21:50:44 -0400

    various setup

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,48 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    url: 'https://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=',
+
+
+    initialize: () => {
+        document.addEventListener('deviceready', app.onDeviceReady);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+
+    onDeviceReady: () => {
+        app.heyListen();
+        app.fetchBoy();
+        url = url +'female';
+    },
+    heyListen: () => {
+        //document.addEventListener();
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    fetchBoy: () => {
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        fetch(app.url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                app.allTheSingleLadies(myJson);
+                //console.log(myJson);
+            });
+    },
 
-        console.log('Received Event: ' + id);
-    }
+    allTheSingleLadies: (myJson) => {
+        let profiles = myJson.profiles;
+        //console.log(profiles);
+        let imgBaseURL = myJson.imgBaseURL;
+        //console.log(imgBaseURL);
+
+        profiles.forEach(profile => {
+            console.log(profile.id);
+            console.log(profile.first);
+            console.log(profile.last);
+            console.log(profile.gender);
+        });
+
+    },
+
 };
 
 app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 var app = {
-    url: 'http://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=',
+    url: 'https://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=',
 
 
     initialize: () => {
@@ -9,15 +9,39 @@ var app = {
 
     onDeviceReady: () => {
         app.heyListen();
-        app.fetchBoy
+        app.fetchBoy();
+        url = url +'female';
     },
     heyListen: () => {
-        document.addEventListener();
+        //document.addEventListener();
     },
 
-    fetchBoy: () => {}
+    fetchBoy: () => {
 
+        fetch(app.url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                app.allTheSingleLadies(myJson);
+                //console.log(myJson);
+            });
+    },
 
+    allTheSingleLadies: (myJson) => {
+        let profiles = myJson.profiles;
+        //console.log(profiles);
+        let imgBaseURL = myJson.imgBaseURL;
+        //console.log(imgBaseURL);
+
+        profiles.forEach(profile => {
+            console.log(profile.id);
+            console.log(profile.first);
+            console.log(profile.last);
+            console.log(profile.gender);
+        });
+
+    },
 
 };
 
